d1097f61ec669515f2d4caf414ee69dcdb36b4e7
AneeqButt786 <aneeqbutt.python@gmail.com>
2025-07-06 06:56:44 +0500

    Added all Python files

---
+++ 1_Hello_World/2_Pattern.py
@@ -0,0 +1,13 @@
+"You can write print() on the next line to add new line in terminal like this:"
+
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10')
+
+"You can also use triple quotes either single or double to prevent multiple print(). "
+print("""
+       1
+     2 3
+   4 5 6 
+7 8 9 10""")
---
+++ 2_Variables_Types/2_Temprature.py
@@ -0,0 +1,13 @@
+"Here is a mini project to convert fahreneit temprature into Celsius."
+
+Fahrenheit = 98.6  # You can change this value to any Fahrenheit temperature
+Celsius = (Fahrenheit - 32) / 1.8
+print(f"Temperature in Fahrenheit: {Celsius}")
+
+
+
+"Here is a mini project to convert Celsius temprature into Fahrenheit."
+
+celsius = 37  # You can change this value to any Celsius temperature
+fahrenheit = (celsius * 1.8) + 32
+print(f"Temperature in Fahrenheit: {fahrenheit}")
