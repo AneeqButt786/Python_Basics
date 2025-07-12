e3fc3e84fdebe5444c4f67ddc746ffdc6b8fff53
AneeqButt786 <aneeqbutt.python@gmail.com>
2025-07-13 00:08:03 +0500

    Added all Python files

--- 3_Lists/3_Favorite_Songs.py
+++
@@ -1,8 +0,0 @@
-"""
-Here is a mini project for your practice.
-
-Make a Songs list for your favorite songs.
-"""
-songs = ["Song A", "Song B", "Song C", "Song D", "Song E"]
-print("Top 3 songs:", songs[:3])
-print("Last song:", songs[-1])
--- 3_Lists/4_Math_Scores.py
+++
@@ -1,9 +0,0 @@
-"""
-Here is a mini project for your practice.
-
-Make a Songs list for your favorite songs.
-"""
-
-scores = [85, 90, 78, 92]
-average = (scores[0] + scores[1] + scores[2] + scores[3]) / 4
-print("Average score:", average)
